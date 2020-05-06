1d7adb3409bda43bcd53c301e98014096663663f
Bella-code <55286642+Bella-code@users.noreply.github.com>
2020-05-06 18:11:21 +0100

    Add files via upload

---
+++ file_utility.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+file_utility.py
+
+Useful functions that manipulate file.
+"""
+
+import os
+import cv2
+
+def createFolder(directory):
+    """Create folder in the specified path unless it already exists.
+    
+    Parameters
+    ----------
+    directory : str
+        The file directory to be created in the specified path
+    Examples
+    --------
+    createFolder('./pics/')
+    """
+    try:
+        # Determine whether the directory exists.
+        if not os.path.exists(directory):
+            # Try to create the directory.
+            os.makedirs(directory)
+    except OSError:
+        print ('Error: Creating directory. ' +  directory)
+        
+def makeVideo(pics_folder,video_name,video_fps=12,T=1000):
+    """Create video using the pictures stored in the specified folder.
+    
+    Parameters
+    ----------
+    pics_folder : str
+        The file location of the stored pictures
+    video_name : str
+        The file name of the video created
+    fps: float, optional
+        Frames per second
+    T: int, optional
+        The total time steps used to create the video
+    """
+    img_array = []
+    for i in range(T):
+        img = cv2.imread(pics_folder+"/{}.png".format(i))
+        height, width, layers = img.shape
+        size = (width,height)
+        img_array.append(img)
+    
+    out = cv2.VideoWriter(video_name+'.mp4',cv2.VideoWriter_fourcc(*'FMP4'), video_fps, size)
+     
+    for i in range(len(img_array)):
+        out.write(img_array[i])
+    out.release()
